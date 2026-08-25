422139d9a80d3d2e118edf7057bf97493185e316
Aleba1988 <alerrandrebarros@yahoo.com.br>
2026-08-25 19:41:49 -0300

    Primeiro commit
    Primeiro commit para verificar a função de commit via github

---
+++ Aula 3.1.py
@@ -0,0 +1,7 @@
+ingresso = input("Você tem ingresso? (sim ou não)")
+aniversariante = input("Você é aniversariante? (sim ou não)")
+
+if ingresso == "sim" or aniversariante == "sim":
+    print("Entrada liberada!")
+else:
+    print("Precisa comprar ingresso.")
---
+++ Aula 3.py
@@ -0,0 +1,7 @@
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura em metros: "))
+
+if idade >= 10 and altura >= 1.40:
+    print("Você pode entrar no brinquedo Radical!")
+else:
+    print("Não pode entrar neste brinquedo.")
